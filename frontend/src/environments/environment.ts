731e53422f2dd870404741858d2044675841dadf
Yong Le He <ygle.he@gmail.com>
2025-07-14 11:58:22 -0400

    feat: setup docker compose and workflow for ci cd deployment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,9 @@
 // This file is for production environment settings.
 export const environment = {
   production: true,
-  stripePublishableKey: '',
-  apiUrl: '',
-  socketUrl: '',
+  apiUrl: 'http://159.203.26.76:3000/api',
+  socketUrl: 'http://159.203.26.76:3000',
+  stripePublishableKey:
+    'pk_test_51RknN3PHZb7FyiFgijP5bJ2SkQFb0YfG2oRzbf6hd2uwMerRIpZrT9ftVS3OG0UqwpAjnIUG0ozAGvZzQyQK7GPJ00rZdel6r1',
   name: 'Foosball',
 };
