d0bfa5793ee925e3ca60e686e3a8c454df0fa49f
Yong Le He <ygle.he@gmail.com>
2025-07-14 14:36:29 -0400

    refactor: point to https endpoints

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 // This file is for production environment settings.
 export const environment = {
   production: true,
-  apiUrl: 'http://159.203.26.76:3000/api',
-  socketUrl: 'http://159.203.26.76:3000',
+  apiUrl: 'https://api.foosball.games/api',
+  socketUrl: 'https://api.foosball.games',
   stripePublishableKey:
     'pk_test_51RknN3PHZb7FyiFgijP5bJ2SkQFb0YfG2oRzbf6hd2uwMerRIpZrT9ftVS3OG0UqwpAjnIUG0ozAGvZzQyQK7GPJ00rZdel6r1',
   name: 'Foosball',
