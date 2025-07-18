3808b2d8dd51b8ded0838cdaa87bd4f70f8518ab
Yong Le He <76537485+yong1le@users.noreply.github.com>
2025-07-17 21:08:16 -0400

    Docker deploy (#17)
    * feat: setup docker compose and workflow for ci cd deployment
    
    * fix: correct frontend dockerfile
    
    * refactor: point to https endpoints
    
    * refactor: change let's encrypt email
    
    * fix: fix angular spa reload 404 issue
    
    * fix: invalid variable refs in workflow file
    
    * fix: don't restart on dev

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,9 @@
 // This file is for production environment settings.
 export const environment = {
   production: true,
-  stripePublishableKey: '',
-  apiUrl: '',
-  socketUrl: '',
+  apiUrl: 'https://api.foosball.games/api',
+  socketUrl: 'https://api.foosball.games',
+  stripePublishableKey:
+    'pk_test_51RknN3PHZb7FyiFgijP5bJ2SkQFb0YfG2oRzbf6hd2uwMerRIpZrT9ftVS3OG0UqwpAjnIUG0ozAGvZzQyQK7GPJ00rZdel6r1',
   name: 'Foosball',
 };
